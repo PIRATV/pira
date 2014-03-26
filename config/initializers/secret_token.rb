3da8ca201c04e280030cc7a2c76a366c60ecd9a3
PIRATV <piratv@inbox.ru>
2014-03-26 10:51:28 +0400

    token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pira::Application.config.secret_key_base = '3be07f090fccb29a3c86e8d25b94e1e258a59a5296d183a274e8869cbfb6f688490dea1221b789b3032448b2abc67686f956ad05b1acdd48c80d62988d8a48d5'
